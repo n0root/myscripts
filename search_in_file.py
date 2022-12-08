23a3b3568c1e0904797523b7bf436207d3d9fde9
n0root <88550551+n0root@users.noreply.github.com>
2022-12-08 11:05:55 -0800

    add word count

--- search_in_file.py
+++ search_in_file.py
@@ -1,14 +1,21 @@
-#################################
+#####################################
 # нумерация всех строк файла
 # и поиск по слову word
 # расположенного по пути path
-################################
+# и вывод количества повторений word
+######################################
 
-path = "/home/user/test/test.txt"
-word = "русский"
-
+path = "/home/user/myscripts/test.txt"
+word = input("Введите слово для поиска: ")
+n=0
+print("\n")
 
 with open(path, mode='r', encoding='mac_cyrillic') as my_file:
 	for number, line in enumerate(my_file):
 		if word in line:
-			print(str(number) + '\t' + line.strip())
+			print(f"{str(number)}\t{line.strip()}")
+			n+=1
+	
+
+	print(f"\n\nСловo \"{word}\" встречалось в файле {n} раз.")
+	
